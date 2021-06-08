864da90391f8ca7f4d4402d0ee38c82092d448c6
Lobaton2020 <andrespipe021028@gmail.com>
2021-06-07 23:52:28 -0500

    Changes register publication

--- src/components/card/newPublication.js
+++ src/components/card/newPublication.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState, useStyles } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
@@ -6,11 +6,64 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import { Input } from '@material-ui/core';
+import { datos } from './../../context/UserContext'
+import { CircularProgress, Fade, Select, Typography } from '@material-ui/core';
 
+//helpers
+function imageToBase64(files = []) {
+  const base64Files = files.map((file) => {
+    return new Promise((resolve, reject) => {
+      try {
+        let fileReader = new FileReader()
+        fileReader.readAsDataURL(file)
+        fileReader.onload = () => {
+          resolve(fileReader.result)
+        }
+      } catch (error) {
+        reject(error)
+      }
+    })
+  })
+  return base64Files
+}
+
+
+//end
+const documentsType = [
+  {
+    id: 1,
+    name: "Necesidad"
+  },
+  {
+    id: 2,
+    name: "Producto"
+  },
+  {
+    id: 3,
+    name: "Servicio"
+  }
+];
+
+const initState = {
+  "userId": datos().userId,
+  "publicationTypeId": 1,
+  "name": "",
+  "price": "",
+  "stock": 0,
+  "contact": "",
+  "description": ""
+};
+const stylesImage = {
+  borderBlockColor: "gray", borderStyle: "solid", width: "100px",
+  margin: "4px"
+};
 export default function FormDialog() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [message, setMessage] = useState({ type: "", "message": "" })
   const [open, setOpen] = React.useState(false);
-
+  const [files, setFiles] = useState([]);
+  const [form, setForm] = useState({ ...initState })
+  const classes = useStyles();
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -18,74 +71,144 @@ export default function FormDialog() {
   const handleClose = () => {
     setOpen(false);
   };
+  const handleChange = async (e) => {
+    const files = await Promise.all(imageToBase64(Array.from(e.target.files)))
+    setFiles(files)
+  }
+  const handleSubmit = (e) => {
 
+    e.preventDefault()
+    if (files.length == 0) alert("Añade imagenes")
+    fetch(window.env.API_ENDPOINT + "users", { method: "POST", body: form })
+      .then(res => res.json())
+      .then((res) => {
+        if ("id" in res) {
+          fetch(window.env.API_ENDPOINT + res.id + "/images", { method: "POST", body: { images: files } })
+            .then((res) => {
+              if (res.ok) {
+                setIsLoading(false)
+                setMessage({ type: "success", message: "Publicacion hecha correctamente" })
+              }
+            })
+        }
+      })
+      .catch(() => {
+        setIsLoading(false)
+        setMessage({ type: "error", message: "Error al crear la publicacion" })
+      })
+
+  }
   return (
     <div>
       <Button variant="outlined" color="primary" onClick={handleClickOpen}>
         Agrega tu publicacion!
       </Button>
       <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+        <DialogTitle id="form-dialog-title">Crear publicacion</DialogTitle>
         <DialogContent>
           <DialogContentText>
-            Ingresa estos datos para publicar tu publicacion!
+            Ingresa estos datos para crear tu publicacion!
           </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="publicationTypeId"
-            label="Tipo de publicación"
-            type="text"
-            fullWidth
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="nombre de su publiación"
-            type="text"
-            fullWidth
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="price"
-            label="price"
-            type="number"
-            fullWidth
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="stock"
-            label="Stock"
-            type="number"
-            fullWidth
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="contact"
-            label="Contacto"
-            type="email"
-            fullWidth
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="description"
-            label="Descripcion"
-            type="email"
-            fullWidth
-          />
+          <Fade in={message.type == "error"}>
+            <Typography color="secondary" className={classes.errorMessage}>
+              {message.message}
+            </Typography>
+          </Fade>
+
+          <Fade in={message.type == "success"}>
+            <Typography color="primary" >
+              Publicacion realizada correctamente.
+            </Typography>
+          </Fade>
+          {files.map((file, i) => (<img key={i} style={stylesImage} src={file} />))}
+          <form id="formSend" onSubmit={handleSubmit}>
+            <Select
+              required
+              fullWidth
+              native
+              onChange={e => setForm({ ...form, publicationTypeId: e.target.value })}
+              value={form.publicationTypeId}
+              inputProps={{
+                name: 'age',
+                id: 'age-native-simple',
+              }}
+            >
+              <option aria-label="None" value="" >--SELECCIONA--</option>
+              {documentsType.map((item) => (<option value={item.id}>{item.name}</option>))}
+            </Select>
+            <TextField
+              required
+              onChange={e => setForm({ ...form, name: e.target.value })}
+              autoFocus
+              margin="dense"
+              id="name"
+              label="nombre de su publiación"
+              type="text"
+              fullWidth
+            />
+            <TextField
+              required
+              onChange={e => setForm({ ...form, price: e.target.value })}
+              autoFocus
+              margin="dense"
+              id="price"
+              label="price"
+              type="number"
+              fullWidth
+            />
+            <TextField
+              required
+              onChange={e => setForm({ ...form, stock: e.target.value })}
+              autoFocus
+              margin="dense"
+              id="stock"
+              label="Stock"
+              type="number"
+              fullWidth
+            />
+            <TextField
+              required
+              onChange={e => setForm({ ...form, contact: e.target.value })}
+              autoFocus
+              margin="dense"
+              id="contact"
+              label="Contacto"
+              type="text"
+              fullWidth
+            />
+            <TextField
+              required
+              onChange={e => setForm({ ...form, description: e.target.value })}
+              autoFocus
+              margin="dense"
+              id="description"
+              label="Descripcion"
+              type="text"
+              fullWidth
+            />
+
+            <Button variant="contained" color="secondary">
+              <label for="photos-here">
+                SUBIR FOTOS
+            </label>
+            </Button>
+            <small>  - Minimo sube una imagen</small>
+          </form>
+          <input id="photos-here" style={{ display: "none" }} type="file" multiple accept="image/*" onChange={handleChange} />
         </DialogContent>
+
+
         <DialogActions>
           <Button onClick={handleClose} color="primary">
             Cancel
           </Button>
-          <Button onClick={handleClose} color="primary">
-            Subscribe
-          </Button>
+          {isLoading ? (
+            <CircularProgress size={26} className={classes.loginLoader} />
+          ) : (
+            <Button form="formSend" type="submit" color="primary">
+              Añadir publicacion
+            </Button>
+          )}
         </DialogActions>
       </Dialog>
     </div>
